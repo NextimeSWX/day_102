87e800efc72486196059aab3e592af447fde20ec
Nextime <evan.lebihan@ecole-89.com>
2022-09-29 10:59:01 +0200

    Exo pairs et impairs

---
+++ is_even.c
@@ -0,0 +1,7 @@
+int is_even(int a) {
+    if (a % 2 ==0) {
+        return 1;
+    }
+    return 0;
+}
+
---
+++ is_odd.c
@@ -0,0 +1,6 @@
+int is_odd(int b) {
+    if (b % 2 == 0) {
+        return 0;
+    }
+    return 1; 
+}
