976520bd86b87f33988ab6d126b4b8ed4fc0e8e8
Nextime <evan.lebihan@ecole-89.com>
2022-09-29 14:07:53 +0200

    Exo Is Between?

---
+++ is_between.c
@@ -0,0 +1,12 @@
+int is_between(int low, int high, int nb) {
+
+    if (nb > low && nb < high) {
+        return 1;
+    }
+    else if(low >= high) {
+        return 0;
+    }
+    else {
+            return 0;
+        }
+}
