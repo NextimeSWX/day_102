b7b1f29c7b2e7df82a07a0fe9e032c9a9c0cd9b8
Nextime <evan.lebihan@ecole-89.com>
2022-09-29 09:46:47 +0200

    Exo printb

--- printb.c
+++ printb.c
@@ -2,15 +2,13 @@
 
 void stu_putchar (char c)
 {
-    write(1, &c, 1);
+x    write(1, &c, 1);
 }
 
 
 int main(void)
 {
-    char b;
-    b = 'b';
-    stu_putchar(b);
+    stu_putchar('b');
     stu_putchar('\n');
     return(0);
 }
