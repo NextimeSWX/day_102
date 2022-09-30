84203ca0a4dcea137ebdafefbda131fa3079c8ba
Nextime <evan.lebihan@ecole-89.com>
2022-09-30 17:00:49 +0200

    Valeur absolue

---
+++ abs.c
@@ -0,0 +1,11 @@
+int stu_abs(int nb) {
+    if (nb < 1) {
+        return (nb * -1);
+    }
+    else if (nb == 0) {
+        return 0;
+    }
+    else {
+        return nb;
+    }
+}
---
+++ test_abs.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int stu_abs(int nb);
+
+int main(void) {
+    return stu_abs(-2);
+}
