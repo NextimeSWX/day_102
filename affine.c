4133bc6c93061976c7f12b3b7c56948102a22c95
Nextime <evan.lebihan@ecole-89.com>
2022-10-07 09:31:49 +0200

    Exo Affine

---
+++ affine.c
@@ -0,0 +1,5 @@
+int affine(int a, int x, int b) {
+    int nb;
+    nb = a*x+b;
+    return nb;
+}
---
+++ test_affine.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int affine(int a , int x , int b);
+int main (void ) {
+    int res ;
+    res = affine(5, 2, 5);
+    printf("%d\n", res);
+}
